6c1e14fa5150b0b8f4dee54b4f69f1b20a3b4407
Michael Miller <bluepixelmike@gmail.com>
2014-10-11 09:45:04 +0000

    Added a utility class - has functionality for downloading files

---
+++ utility.rb
@@ -0,0 +1,42 @@
+require 'uri'
+require 'open-uri'
+
+# Extracts the filename from a URL
+# @param url [String] URL of the file
+# @return [String] Filename pulled from the URL
+def url_filename (url)
+  uri = URI.parse(url)
+  File.basename(uri.path)
+end
+
+# Downloads the contents at a URL into a destination directory
+# @param url  [String] URL of the file
+# @param dest [String] Destination directory to save the file in
+# @return [null]
+def download (url, dest)
+  wrap_download(url, dest) {|url| open(url).read}
+end
+
+# Downloads the contents at a URL that require authentication into a destination directory
+# @param url  [String] URL of the file
+# @param dest [String] Destination directory to save the file in
+# @param user [String] HTTP basic authentication username
+# @param pass [String] HTTP basic authentication password
+# @return [null]
+def download_auth (url, dest, user, pass)
+  wrap_download(url, dest) {|url| open(url, :http_basic_authentication => [user, pass]).read}
+end
+
+# Sets up the output file to write content to it from a URL
+# @url [String] URL of the file
+# @dest [String] Destination directory to save the file in
+# @block Block returning the contents of the URL
+# @return [null]
+def wrap_download (url, dest, &block)
+  filename = url_filename(url)
+  filepath = "#{dest}/#{filename}"
+  File.open(filepath, 'wb') do |file|
+    file.write block.call(url)
+  end
+end
+
