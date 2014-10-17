942613664b48e6ce71410d4d492a2b1a607a2274
Michael Miller <bluepixelmike@gmail.com>
2014-10-17 15:46:20 -0600

    Added loading of client and server mod lists

--- lib/pockit/modpack.rb
+++ lib/pockit/modpack.rb
@@ -24,13 +24,15 @@ module Pockit
     attr_reader :website
     
     # Creates a new modpack instance
-    def initialize (name, description, version, mc_version, author, website)
+    def initialize (name, description, version, mc_version, author, website, client_mods, server_mods)
       @name        = name
       @description = description
       @version     = version
       @mc_version  = mc_version
       @author      = author
       @website     = website
+      @client_mods = client_mods
+      @server_mods = server_mods
     end
     
     # Loads modpack information from a file
@@ -46,8 +48,21 @@ module Pockit
       mc_version  = data['mc_version']
       author      = data['author']
       website     = data['website']
-      Pockit::Modpack.new(name, description, version, mc_version, author, website)
+      client_mods = load_modlist(data['client_contents'])
+      server_mods = load_modlist(data['server_contents'])
+      Pockit::Modpack.new(name, description, version, mc_version, author, website, client_mods, server_mods)
     end
+    
+    # Retrieves a list of mod IDs from a modlist file
+    # @param path [String] Path to the file to load from
+    # @return [Array<String>] Collection of mod IDs
+    def self.load_modlist (path)
+      json = File.read(path)
+      data = JSON.parse(json)
+      data['mods'].to_a
+    end
+    
+    private_class_method :load_modlist
     
   end
   
